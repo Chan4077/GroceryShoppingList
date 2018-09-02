dada657a4b462e55df31e3f67d3813f91a7b78e0
Edric Chan <edric.chan.1997@gmail.com>
2018-09-02 21:49:17 +0800

    Output Python and Tkinter versions in about dialog

--- about_dialog.py
+++ about_dialog.py
@@ -2,6 +2,7 @@
 from tkinter import *
 from tkinter import simpledialog as dialog
 from tkinter import messagebox as mbox
+from sys import version
 
 class AboutDialog(dialog.Dialog):
 
@@ -10,7 +11,10 @@ class AboutDialog(dialog.Dialog):
         # self.maxsize(width=200)
         self.title("About Grocery List")
         Label(master, text="Made for the purposes of Google's Science Fair 2018").grid(row=0, sticky=W)
-        Label(master, text="Made using Python 3 and Tkinter").grid(row=1, sticky=W)
+        Label(master, text="Python version: {}".format(version)).grid(row=1, sticky=W)
+        Label(master, text="Tkinter version: {}".format(TkVersion)).grid(row=2, sticky=W)
+        Label(master, text="Copyright Edric Chan. All rights reserved").grid(row=3, sticky=W)
+        Label(master, text="This project is licensed with the MIT license.").grid(row=4, sticky=W)
         self.resizable(width=False, height=False)
     def buttonbox(self):
         pass
